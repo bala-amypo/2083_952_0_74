3c9d09840ee76dc67ed69fa7c33c9e5334f80260
bala-amypo <bala@amypo.in>
2025-12-17 10:11:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -13,6 +13,10 @@ public class StudentServiceImpl implements StudentService {
     public Student poststudent(Student st){
         return stdrepo.save(st);
     }
+    @Override
+    public List<Student> getAllStudent(){
+
+    }
 }
 
 
