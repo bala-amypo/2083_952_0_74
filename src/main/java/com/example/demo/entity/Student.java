2d50197c8dea9e9a9c45b2a811e074e7f565f4cd
bala-amypo <bala@amypo.in>
2025-12-17 04:56:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 
 public class Student{
     @Id
+    @GeneratedValue(strategy=GeneraionType.IDENTITY)
     private Long id;
     private String name;
     private String dept;
